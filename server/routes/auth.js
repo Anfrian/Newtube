71c0f3c6e997b8baf63dd9b3f2b1202e12e77865
Anfrian <gorkemkadir14@hotmail.com>
2022-08-18 06:50:00 +0300

    signup&signin

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { signup } from "../controllers/auth.js"
+import { signup, signin } from "../controllers/auth.js"
 
 const router = express.Router()
 
 router.post("/signup", signup)
 
-router.post("/signin", )
+router.post("/signin", signin)
 
 router.post("/google", )
 
