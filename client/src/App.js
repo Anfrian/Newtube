fe15f6517c3543aa8ea419d3ae1c28a33ede00f6
Anfrian <gorkemkadir14@hotmail.com>
2022-08-25 07:40:35 +0300

    ui & client

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import Menu from "./Components/Menu";
+import Navbar from "./Components/Navbar";
+
+const Container = styled.div`
+  display: flex;
+  
+`
+
+const Main = styled.div `
+  flex: 7;
+`
+
+const Wrapper = styled.div `
+
+`
+
+function App() {
+  return (
+    <Container>
+      <Menu>
+        
+      </Menu>
+      <Main>
+        <Navbar></Navbar>
+        <Wrapper>
+          Video Cards
+        </Wrapper>
+      </Main>
+    </Container>
+  );
+}
+
+export default App;
