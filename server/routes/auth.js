12ef729f9aac7501c91f022db7273e60c434856b
Anfrian <gorkemkadir14@hotmail.com>
2022-08-30 15:00:21 +0300

    redux & google

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signup, signin } from "../controllers/auth.js"
+import { signup, signin, googleAuth } from "../controllers/auth.js"
 
 const router = express.Router()
 
@@ -7,6 +7,6 @@ router.post("/signup", signup)
 
 router.post("/signin", signin)
 
-router.post("/google", )
+router.post("/google", googleAuth)
 
 export default router;
